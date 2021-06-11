d76a0f82ff8f54d9d208a61039a03f6caf6a176d
Mayu Satori <satorimayu@gmail.com>
2021-06-11 05:09:13 -0300

    Att exercise of day 6.1 with more style and with the last question solved

--- exercises/B6/6.1/exercise-2/script.js
+++ exercises/B6/6.1/exercise-2/script.js
@@ -1,15 +1,39 @@
+function converterObjParaStr(obj) {
+  let str='';
+  for(let index in obj){
+    if (typeof obj[index] == 'string') {
+        str += index + ': ' + obj[index]+'; </br>';
+    } else {
+        str += index + ': { </br>' + print(obj[index]) + '}';
+    }
+  }
+  return str;
+};
+
+// Nesse evento, eu capturo todos os inputs e coloco o value deles em um objeto, usando o name dos mesmos inputs como a chave para os valores armazenados no objeto. depois, uso a função acima para converter o objeto para String, e aplico na div
+
 document.querySelector('#enviar').addEventListener('click', (evento) => {
   evento.preventDefault();
   const inputs = document.getElementsByTagName('input');
   const select = document.getElementById('selectEstado');
-  const textArea = document.getElementById('textCurriculo');
+  const textArea = document.getElementById('textCurriculo')
+  const resultadoDiv = document.getElementById('resultadoDiv');
+  const resultadoDivPai = document.getElementById('resultadoDivPai');
+  let resultado = {};
   for (let index = 0; index < inputs.length; index += 1) {
-    console.log(inputs[index].value);
+    // console.log(inputs[index].value);
+    resultado[inputs[index].name]=inputs[index].value;
     if (inputs[index].value === '') {
-      alert('Campo vazio!');
-      break;
+      return alert('Campo vazio!');
     }
   }
-  console.log(select.options[select.selectedIndex].value);
-  console.log(textArea.value);
+  // console.log(select.options[select.selectedIndex].value);
+  // console.log(textArea.value);
+  resultado[select.name]=select.options[select.selectedIndex].value;
+  resultado[textArea.name]=textArea.value;
+  // console.log(resultado);
+  //resultadoDiv.innerHTML = JSON.stringify(resultado, null, 4);
+  resultadoDiv.innerHTML = converterObjParaStr(resultado);
+  resultadoDivPai.style.display = 'block';
 });
+
