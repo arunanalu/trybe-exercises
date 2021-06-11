76562dc659f4e4d334ba51426767a54e357821de
Mayu Satori <satorimayu@gmail.com>
2021-06-11 04:12:38 -0300

    att exercise of day 6.1 with JS and style

--- exercises/B6/6.1/exercise-2/script.js
+++ exercises/B6/6.1/exercise-2/script.js
@@ -1,5 +1,15 @@
 document.querySelector('#enviar').addEventListener('click', (evento) => {
   evento.preventDefault();
+  const inputs = document.getElementsByTagName('input');
+  const select = document.getElementById('selectEstado');
+  const textArea = document.getElementById('textCurriculo');
+  for (let index = 0; index < inputs.length; index += 1) {
+    console.log(inputs[index].value);
+    if (inputs[index].value === '') {
+      alert('Campo vazio!');
+      break;
+    }
+  }
+  console.log(select.options[select.selectedIndex].value);
+  console.log(textArea.value);
 });
-
-
