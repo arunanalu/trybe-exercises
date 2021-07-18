d91b3bbdeae8ee0d07495dbe5ab94ec735d8d21b
Mayu <mayusatori@gmail.com>
2021-07-18 09:01:18 -0300

    Add exercise 3 solved

--- exercises/B10/10.2/exercise-2/findAnimals.test.js
+++ exercises/B10/10.2/exercise-2/findAnimals.test.js
@@ -20,22 +20,24 @@ const Animals = [
   const getListAnimals = (type) => (
     findAnimalsByType(type).then(list => list)
   );
-  
-  describe('Quando o tipo do animal existe, testando promisse', () => {
-    test('Retorna a lista de animais', () => {
-      expect.assertions(2);
-      return findAnimalsByType('Dog').then((listDogs) => {
-        expect(listDogs[0].name).toEqual('Dorminhoco');
-        expect(listDogs[1].name).toEqual('Soneca');
+
+  describe('Testes com promise', () => {
+    describe('Quando o tipo do animal existe, testando promisse', () => {
+      test('Retorna a lista de animais', () => {
+        expect.assertions(2);
+        return findAnimalsByType('Dog').then((listDogs) => {
+          expect(listDogs[0].name).toEqual('Dorminhoco');
+          expect(listDogs[1].name).toEqual('Soneca');
+        });
       });
     });
-  });
-
-  describe('Quando o tipo do animal não existe, testando promisse', () => {
-    test('Retorne a lista de animais', () => {
-      return getListAnimals('Lion').catch(error =>
-        expect(error).toEqual({ error: "Não possui esse tipo de animal." })
-      );
+  
+    describe('Quando o tipo do animal não existe, testando promisse', () => {
+      test('Retorne a lista de animais', () => {
+        return getListAnimals('Lion').catch(error =>
+          expect(error).toEqual({ error: "Não possui esse tipo de animal." })
+        );
+      });
     });
   });
 
@@ -54,14 +56,22 @@ const Animals = [
     });
   });
 
-  describe('Teste com promise e matcher reject', () => {
+  describe('Teste com promise e matcher resolve/reject', () => {
+    test('Retorne a lista de animais', () => {
+      const listDogs = [
+        { name: 'Dorminhoco', age: 1, type: 'Dog' },
+        { name: 'Soneca', age: 2, type: 'Dog' },
+      ]
+      expect.assertions(1);
+      return expect(getListAnimals('Dog')).resolves.toEqual(listDogs)
+    });
     test('Retorna um erro', () => {
       // expect.assertions(1);
       return expect(getListAnimals('Lion')).rejects.toEqual({ error: 'Não possui esse tipo de animal.' })
     });
   })
 
-  describe('Teste com matcher resolve e async/await', () =>{
+  describe('Teste com async/await e matcher resolve/reject', () =>{
     test('Retorne a lista de animais', async () => {
       const listDogs = [
         { name: 'Dorminhoco', age: 1, type: 'Dog' },
@@ -69,5 +79,9 @@ const Animals = [
       ]
       // expect.assertions(1);
       await expect(getListAnimals('Dog')).resolves.toEqual(listDogs)
+    });
+    test('Retorna um erro', async () => {
+      expect.assertions(1);
+      await expect(getListAnimals('Lion')).rejects.toEqual({ error: 'Não possui esse tipo de animal.' })
     });
   });
---
+++ exercises/B10/10.2/exercise-3/userData.test.js
@@ -0,0 +1,51 @@
+const {
+  findUserById,
+  getUserName,
+} = require('./userData');
+
+describe('Testes sem os matchers resolve/reject', () => {
+  describe('Testes que tratam a promise', () => {
+    it('A função getUserName deve retornar o nome correto', () => {
+      expect.assertions(1);
+      return getUserName(4).then((name) => {
+        expect(name).toBe('Mark')
+      });
+    });
+    it('A função getUserName deve retornar um erro caso o id não exista', () => {
+      expect.assertions(1);
+      return getUserName(7).catch((erro) => {
+        expect(erro).toEqual({"error": "User with 7 not found."});
+      });
+    })
+  })
+  
+  describe('Testes com async/await', () => {
+    it('A função getUserName deve retornar o nome correto e retorar erro caso o id não exista', async () => {
+      try {
+        const name = await getUserName(4);
+        expect(name).toBe('Mark');  
+      } catch (erro) {
+        expect(erro).toEqual({"error": "User with 7 not found."});
+      }
+    });
+  });
+});
+
+describe('Testes com resolve/reject', () => {
+  describe('Testes que tratam a promise', () => {
+    it('A função getUserName deve retornar o nome correto', () => {
+      return expect(getUserName(4)).resolves.toBe('Mark')
+    });
+    it('A função getUserName deve retornar um erro caso o id não exista', () => {
+      return expect(getUserName(7)).rejects.toEqual({"error": "User with 7 not found."})
+    })
+  })
+  describe('Testes com async/await', () => {
+    it('A função getUserName deve retornar o nome correto', async () => {
+      await expect(getUserName(4)).resolves.toBe('Mark')
+    });
+    it ('A função getUserName deve retornar um erro caso o id não exista', async () => {
+      await expect(getUserName(7)).rejects.toEqual({"error": "User with 7 not found."})
+    });
+  });
+});
