28386280ee3962afc2e0f8ab50471bf840672d01
Mayu <mayusatori@gmail.com>
2021-07-20 23:43:40 -0300

    Add exercises of day

---
+++ exercises/B11/11.1/exercise-2/first-react-exercise/src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Descricao extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Mayu Satori</h1>
+        <p>New React developer</p>
+      </div>
+    )
+  }
+}
+
+export default Descricao;
